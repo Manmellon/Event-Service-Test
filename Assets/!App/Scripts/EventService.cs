6f6565f186b69afa7a557530712f791f8fd1ea25
Manmellon <24492231+Manmellon@users.noreply.github.com>
2023-04-11 21:32:20 +0300

    Send queue, clean it only if result was 200, and save after it and after every TrackEvent()

--- Assets/!App/Scripts/EventService.cs
+++ Assets/!App/Scripts/EventService.cs
@@ -4,17 +4,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using Proyecto26;
+using System.IO;
 
 [Serializable]
 public struct Event
 {
     public string type;
     public string data;
+    public bool sendingNow;
 
-    public Event(string type, string data)
+    public Event(string type, string data, bool sendingNow = false)
     {
         this.type = type;
         this.data = data;
+        this.sendingNow = sendingNow;
     }
 }
 
@@ -39,7 +42,7 @@ public class EventService : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        LoadEventsQueue();
     }
 
     // Update is called once per frame
@@ -47,6 +50,7 @@ public class EventService : MonoBehaviour
     {
         if (Time.time >= prevSendTime + cooldownBeforeSend)
         {
+            prevSendTime = Time.time;
             SendEvents();
         }
     }
@@ -54,10 +58,62 @@ public class EventService : MonoBehaviour
     public void TrackEvent(string type, string data)
     {
         eventsQueue.Add(new Event(type, data));
+
+        SaveEventsQueue();
     }
 
     public void SendEvents()
     {
-        Debug.Log(JsonUtility.ToJson(eventsQueue));
+        for (int i = 0; i < eventsQueue.Count; i++)
+        {
+            eventsQueue[i] = new Event(eventsQueue[i].type, eventsQueue[i].data, true);
+        }
+
+        string body = JsonHelper.ArrayToJsonString(eventsQueue.ToArray());
+        body = body.Replace("Items", "events");
+        Debug.Log(body);
+
+        RestClient.Post(serverUrl, body)
+        .Then(res =>
+        {
+            if (res.StatusCode == 200)
+            {
+                eventsQueue.RemoveAll( (x) => x.sendingNow == true );
+                SaveEventsQueue();
+            }
+            else
+            {
+                Debug.LogWarning(res.StatusCode);
+            }
+        })
+        .Catch(err =>
+        {
+            Debug.LogError(err.Message);
+        });
+    }
+
+    public void SaveEventsQueue()
+    {
+        using (StreamWriter writer = new StreamWriter("eventsQueue.json"))
+        {
+            writer.WriteLine(JsonHelper.ArrayToJsonString(eventsQueue.ToArray()));
+        }
+    }
+
+    public void LoadEventsQueue()
+    {
+        try
+        {
+            using (StreamReader reader = new StreamReader("eventsQueue.json"))
+            {
+                Event[] array = JsonHelper.FromJsonString<Event>(reader.ReadToEnd());
+                eventsQueue = new List<Event>();
+                eventsQueue.AddRange(array);
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            Debug.LogWarning(e.Message);
+        }
     }
 }
